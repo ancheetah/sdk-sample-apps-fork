c754e2e9c22337b19e096f4d90419210c113e374
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-02-25 14:22:44 -0500

    request from protected endpoint

--- reactjs-todo/client/utilities/request.js
+++ reactjs-todo/client/utilities/request.js
@@ -9,6 +9,7 @@
  * of the MIT license. See the LICENSE file for details.
  */
 // eslint-disable-next-line no-unused-vars
+import { HttpClient } from '@forgerock/javascript-sdk';
 import { API_URL, DEBUGGER } from '../constants';
 
 /**
@@ -21,12 +22,15 @@ import { API_URL, DEBUGGER } from '../constants';
 export default async function apiRequest(resource, method, data) {
   let json;
   try {
-    const response = await fetch(`${API_URL}/${resource}`, {
-      body: data && JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await HttpClient.request({
+      url: `${API_URL}/${resource}`,
+      init: {
+        body: data && JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: method,
       },
-      method: method,
     });
     if (!response.ok) {
       throw new Error(`Status ${response.status}: API request failed`);
